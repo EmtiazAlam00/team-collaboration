00b781ccbf4cfe2b83bfe61092f416b2cac143f2
dnkem <106627182+dnkem@users.noreply.github.com>
2024-04-05 03:00:04 -0400

    Update session.cpp

--- COMP3004_Team46/src/session.cpp
+++ COMP3004_Team46/src/session.cpp
@@ -4,13 +4,16 @@ Session::Session(int ID, int l, QDateTime date_time){
     id = ID;
     length = l;
     dt = date_time;
+    timer = new QTimer(this);
 }
 
 Session::~Session(){
-
+    delete timer;
 }
 
 //getters
 int Session:: getID() { return id;}
+QTimer* Session:: getTimer() { return timer;}
 int Session:: getLength() { return length;}
 QDateTime Session:: getStartTime() { return dt;}
+
