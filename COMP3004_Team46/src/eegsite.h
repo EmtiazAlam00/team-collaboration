dd5c58808722a177a1bf684d36ce9541a0fe57c2
kaitlynwarhurst <122323138+kaitlynwarhurst@users.noreply.github.com>
2024-04-10 23:37:13 -0400

    Update eegsite.h
    Added header comments

--- COMP3004_Team46/src/eegsite.h
+++ COMP3004_Team46/src/eegsite.h
@@ -9,6 +9,24 @@
 #include <fftw3.h>
 
 using namespace std;
+/* Class Purpose: Creates simulated waveforms for testing purposes and calculates the dominant frequency based on an inserted waveform.
+ *
+ * Data Members:
+ * - bool connected: Is set to true if it is connected to an actual EEG- so for simulation and testing purposes the default is false
+ * - const PI value - has value of pi for the waveform calculations
+ * - const static bounds (e.g. BETA_MIN_FREQ and BETA_MAX_FREQ) to set upper and lower bounds for the different bands (theta, beta, alpha and delta)
+ * - static SAMPLING_RATE: the sampling rate of the EEG- set to 250 as that is a very common sampling rate for eegs
+ * Class Functions:
+ * - void getSignal(): gets the waveform from the EEG- if connected to an actual EEG this would be connected to the function to get the waveform directly otherwise it creates and generates the test waveform
+ * - static float calculateDominant(vector<double>& v): Calculates the dominant frequency by doing a Fast Fourier Transform, and a power spectrum analysis
+ * - deliverTreatment(double treatmentFreq, long duration): if connected to an actual EEG it would call a function to deliver the treatment frequency for the specified duration, currently just a dummy function and prints that it delivers treatment to the console
+ * - generateWaveforms(): simulates waveforms by summing sin waves for the different freqeuncy bands- the different bands have different weights (randomly generated) and random frequency for the different bands
+ * - generateEEGSignal(): generates a single signal, several of these signals are used to build the wave form- serves as a helper function
+ * - generateWeights and generateFrequencies are helper functions to randomly generate the weights and frequencies for each band
+ * - 
+ */
+
+
 
 class EegSite{
     public:
