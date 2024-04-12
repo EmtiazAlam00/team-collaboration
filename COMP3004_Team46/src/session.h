0c091378455ce2c1fae33b308696a2cbecd2473b
dnkem <106627182+dnkem@users.noreply.github.com>
2024-04-12 00:25:27 -0400

    Update session.h

--- COMP3004_Team46/src/session.h
+++ COMP3004_Team46/src/session.h
@@ -8,23 +8,23 @@
 #include <QDebug>
 
 /* Class Purpose: Stores the data from a LENS treatment session on the Nearest device. 
-The data includes the start time, and the before and after baseline average frequencies. 
- 
- Data Members:
-- QDateTime startTime: Stores the session’s start time.
-- QVector<float> beforeBaselines: Store the before baseline average frequencies in a vector.
-- QVector<float> afterBaselines: Store the after baseline average frequencies in a vector.
-  
- 
- Class Functions:
-- Session(const QDateTime& startTime): Initializes the session’s starting time and the before and 
-after baseline average frequency vectors are set to size 7.
-- print(): Prints the session’s start time, before and after baseline average frequencies values.
-- getStartTime(): Returns the session’s start time.
-- getBaselineAfter(int num): Returns a float value at index num from the after baseline average frequency vector.
-- setBefore(int i, float baselineVal): Sets the baselineVal at index i in the before baseline average frequency vector.
-- setAfter(int i, float baselineVal): Sets the baselineVal at index i in the after baseline average frequency vector.
-  
+* The data includes the start time, and the before and after baseline average frequencies. 
+* 
+* Data Members:
+* - QDateTime startTime: Stores the session’s start time.
+* - QVector<float> beforeBaselines: Stores the before baseline average frequencies in a vector.
+* - QVector<float> afterBaselines: Stores the after baseline average frequencies in a vector.
+*   
+*  
+*  Class Functions:
+* - Session(const QDateTime& startTime): Initializes the session’s starting time and the before and 
+* after baseline average frequency vectors are set to size 7.
+* - print(): Prints the session’s start time, before and after baseline average frequencies' values.
+* - getStartTime(): Returns the session’s start time.
+* - getBaselineAfter(int num): Returns a float value at index num from the after baseline average frequency vector.
+* - setBefore(int i, float baselineVal): Sets the baselineVal at index i in the before baseline average frequency vector.
+* - setAfter(int i, float baselineVal): Sets the baselineVal at index i in the after baseline average frequency vector.
+*   
 */
 
 class Session: public QObject{
