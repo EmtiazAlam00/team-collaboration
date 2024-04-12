5bbbe0c8730b38657576f25ead148a59ea16ef6f
dnkem <106627182+dnkem@users.noreply.github.com>
2024-04-12 00:29:01 -0400

    Update session.cpp

--- COMP3004_Team46/src/session.cpp
+++ COMP3004_Team46/src/session.cpp
@@ -2,36 +2,28 @@
 
 Session::Session( const QDateTime& startTime)
     :  startTime(startTime), beforeBaselines(7,0.0), afterBaselines(7, 0.0){
-
 }
 
-Session::~Session(){
+Session::~Session(){}
 
-}
-
-void Session::print()
-{
+void Session::print(){
     qDebug()<< startTime.toString();
     qDebug()<< "Befores: "<< beforeBaselines;
     qDebug()<< "Afters: " << afterBaselines;
 }
 
-//getters
+// getters & setters
 
+QDateTime Session::getStartTime() {return startTime;}
 
-QDateTime Session::getStartTime() { return startTime;}
 float Session::getBaselineAfter(int num){
     return afterBaselines[num];
 }
 
-void Session::setBefore(int i, float baselineVal)
-{
+void Session::setBefore(int i, float baselineVal){
     beforeBaselines[i] = baselineVal;
 }
 
-void Session::setAfter(int i, float baselineVal)
-{
+void Session::setAfter(int i, float baselineVal){
     afterBaselines[i] = baselineVal;
 }
-
-
